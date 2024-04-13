2e1e7e16db2c5d16dacc4658c2939e0ab5520d1a
lucaoskaique <lucas.kaique175@hotmail.com>
2024-04-13 16:02:43 -0300

    Adicionado componente Cube e ThreeElement, e corrigido página Landing.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
-
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import "../styles/pages/landing.css";
+import ThreeElement from "../components/ThreeElement";
+import * as THREE from "three";
+import { extend } from "@react-three/fiber";
+import { ACESFilmicToneMapping, SRGBColorSpace } from "three";
+import Discoballs from "../components/Discoballs";
+import Overlay from "../components/Overlay";
 
 function Landing() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <main>
-          <h1>MAPA DE RAVE</h1>
-          <p>Descubra a Batida do Underdroung</p>
-        </main>
-
-        <Link to="/raves" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </Link>
-      </div>
-    </div>
+    <>
+      <Suspense fallback={null}>
+        <Discoballs speed={1} />
+      </Suspense>
+      <Overlay />
+    </>
   );
 }
 
