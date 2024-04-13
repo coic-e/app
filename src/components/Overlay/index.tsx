a347e9f54ece1510f0d63d9b85becab390b86693
lucaoskaique <lucas.kaique175@hotmail.com>
2024-04-13 16:35:15 -0300

    Corrigido estilo da página de entrada e atualizado conteúdo do componente Overlay

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -2,23 +2,22 @@ export default function Overlay() {
   return (
     <div>
       <div>
-        <h1>COICE —</h1>
-        <p>Exploring percussive sounds —</p>
+        <h1>MAPA DE RAVE</h1>
+        <p>Descubra a Batida do Underdroung</p>
       </div>
       <div>
-        COICE surge em 2020 como um podcast voltado para música eletrônica.
-        <br />
-        Focada em sons percussivos, valorizando a sonoridade da América Latina e
-        brasileira.
-        <br />
-        Enfatizando a importância do protagonismo local na cena da música
-        eletrônica.
-        <br />
-        A festa se propõe como uma interseção entre música, programação e artes
-        visuais/performance.
-        <br />
+        Seja bem-vindo à nossa família rave! No Mapa de Rave, você não apenas
+        encontra festas; você descobre portais para experiências inesquecíveis
+        onde a batida da música eletrônica pulsa em harmonia com seu espírito
+        aventureiro.
       </div>
       <div>
+        <span>
+          Desenvolvido por <a href="">COICE PRODUÇÕES</a>
+        </span>
+        <span>
+          Facebook <a href="https://www.facebook.com/coic.e">Facebook</a>
+        </span>
         <span>
           Instagram <a href="https://www.instagram.com/coic_e/">@coic_e</a>
         </span>
@@ -26,12 +25,6 @@ export default function Overlay() {
           Soundcloud <a href="https://soundcloud.com/coic_e">Soundcloud</a>
         </span>
       </div>
-      <div>
-        <div />
-        <div />
-        <div />
-      </div>
-      {/* <VelvetBanana /> */}
     </div>
   );
 }
