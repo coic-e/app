690fdd9f24370ef5c87c171a0edef2adf98a355b
lucaoskaique <lucas.kaique175@hotmail.com>
2024-04-14 01:27:55 -0300

    Corrigido estilo da página de entrada e adicionado link para a página de raves

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,17 +1,27 @@
+import { Link } from "react-router-dom";
+import { BottomCenter, Button, Container, TopCenter } from "./styles";
+import { FiArrowRight } from "react-icons/fi";
+
 export default function Overlay() {
   return (
-    <div>
-      <div>
+    <Container>
+      <TopCenter>
         <h1>MAPA DE RAVE</h1>
         <p>Descubra a Batida do Underdroung</p>
-      </div>
-      <div>
-        Seja bem-vindo à nossa família rave! No Mapa de Rave, você não apenas
-        encontra festas; você descobre portais para experiências inesquecíveis
-        onde a batida da música eletrônica pulsa em harmonia com seu espírito
-        aventureiro.
-      </div>
-      <div>
+        <div>
+          Seja bem-vindo à nossa família rave! No Mapa de Rave, você não apenas
+          encontra festas; você descobre portais para experiências inesquecíveis
+          onde a batida da música eletrônica pulsa em harmonia com seu espírito
+          aventureiro.
+        </div>
+        <Button>
+          <Link to="/raves" >
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+          </Link>
+        </Button>
+      </TopCenter>
+
+      <BottomCenter>
         <span>
           Desenvolvido por <a href="">COICE PRODUÇÕES</a>
         </span>
@@ -24,7 +34,7 @@ export default function Overlay() {
         <span>
           Soundcloud <a href="https://soundcloud.com/coic_e">Soundcloud</a>
         </span>
-      </div>
-    </div>
+      </BottomCenter>
+    </Container>
   );
 }
