e484c32626e2b5b56e5a2a5ee0700a2d845f05d9
lucaoskaique <lucas.kaique175@hotmail.com>
2024-04-14 01:27:47 -0300

    Corrigido estilo da página de entrada e adicionado importação de fontes externas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-
-import "./styles/global.css";
 import "leaflet/dist/leaflet.css";
 
 import { router } from "./routes";
 import { RouterProvider } from "react-router-dom";
-
+import GlobalStyles from "./styles/global";
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
